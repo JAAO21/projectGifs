7958ca208aab3026823528d47ddd440ae7a9bfa0
JAAO21 <jorgeandres2104@gmail.com>
2026-01-20 22:34:39 -0500

    fix: bdmoongose to db mongo altas config

--- server/bdmoongose.js
+++ server/bdmoongose.js
@@ -1,14 +1,29 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const initDbM = async () => {
-    try {
-        mongoose.set('strictQuery', true);
-        await mongoose.
-            connect(`${process.env.DBM_TYPE}://${process.env.DBM_HOST}:${process.env.PORTM_DB}/${process.env.DBM_NAME}`)
-            .then(db => console.log("Db is conected moongose",db.connection.name));
-    } catch (error) { console.log(error) }
-}
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose
+      .connect(
+        `${process.env.DBM_TYPE}://${process.env.DBM_HOST}:${process.env.PORTM_DB}/${process.env.DBM_NAME}`,
+      )
+      .then((db) => console.log("Db is conected moongose", db.connection.name));
+  } catch (error) {
+    console.log(error);
+  }
+};
+const initDbMAtlas = async () => {
+  try {
+    console.log("Revisando URI:", process.env.MONGODB_URI);
+    mongoose.set("strictQuery", true);
+    await mongoose
+      .connect(process.env.MONGODB_URI)
+      .then((db) => console.log("Db is conected moongose", db.connection.name));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = initDbM;
+module.exports = initDbMAtlas;
