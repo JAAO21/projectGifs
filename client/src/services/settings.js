31521dbafc89b4108d83d1d82c0673ffa73713d8
JAAO21 <jorgeandres2104@gmail.com>
2026-01-25 12:40:37 -0500

    feat: link backend using environment variable

--- client/src/services/settings.js
+++ client/src/services/settings.js
@@ -1,5 +1,5 @@
-export const API_KEY = 'KjUdJXfxiFjmeAJdcfQBW9S14ZCAMLQ7';
+export const API_KEY = "KjUdJXfxiFjmeAJdcfQBW9S14ZCAMLQ7";
 
-export const API_Url = 'https://api.giphy.com/v1'
+export const API_Url = "https://api.giphy.com/v1";
 
-export const API='http://localhost:3500'
+export const API = process.env.REACT_APP_API_URL || "http://localhost:3500";
